7ac345fe2ae9c789086fa7bf970167e790ae3a82
jakhongir375 <151602041+jakhongir375@users.noreply.github.com>
2024-03-29 11:31:52 +0500

    Add files via upload

---
+++ API/script.js
@@ -0,0 +1,28 @@
+const wrap = document.querySelector('.wrap');
+const loader = document.querySelector('.loader');
+
+const func = async () => {
+  const response = await fetch('https://dummyjson.com/users');
+
+  const details = await response.json();
+  console.log(details.users);
+  details.users.map((el) => {
+    wrap.innerHTML += `
+        <div class="card">
+            <img src=${el.image} alt="">
+            <h2>${el.firstName} ${el.lastName} </h2>
+            <h3>${el.phone} </h3>
+            <p>${el.age} </p>
+            <b>${el.birthDate} </b>
+            <h4></h4>
+        </div>
+        
+        `;
+  });
+};
+
+func();
+
+setTimeout(() => {
+  loader.remove();
+}, 3000);
